9e320281a4ffe148e811b6278a0cd3e63d941b52
lumos-ap <39933014+lumos-ap@users.noreply.github.com>
2020-12-20 22:32:28 +0530

    Update SpecialisationService.java

--- src/main/java/com/iiitb/academia/service/SpecialisationService.java
+++ src/main/java/com/iiitb/academia/service/SpecialisationService.java
@@ -1,4 +1,30 @@
 package com.iiitb.academia.service;
 
+import com.iiitb.academia.bean.Specialisation;
+import com.iiitb.academia.dao.SpecialisationDAO;
+import com.iiitb.academia.dao.impl.SpecialisationDAOImpl;
+
+import java.util.List;
+
 public class SpecialisationService {
+    SpecialisationDAO specialisationDAO=new SpecialisationDAOImpl(); 
+        
+        public boolean addSpecialisation(Specialisation specialisation) {
+            return false;
+        }
+
+        
+        public List<Specialisation> getSpecialisationDetails() {
+            return null;
+        }
+        
+        public Specialisation getSpecialisationDetailsById(Integer id) {
+            return null;
+        }
+        
+        public Specialisation getSpecialisationDetailsByCode(String code) {
+            return null;
+        
+    }
 }
+
