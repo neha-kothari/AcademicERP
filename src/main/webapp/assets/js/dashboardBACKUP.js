92c2317da78eb90dd69ade95c11771c0c667701a
neha-kothari <neha.kothari05@gmail.com>
2020-12-21 21:55:44 +0530

    changes for session function call in try-catch + display of table

--- src/main/webapp/assets/js/dashboardBACKUP.js
+++ src/main/webapp/assets/js/dashboardBACKUP.js
@@ -227,6 +227,26 @@ function populateTable(courseData) {
 function init() {
     dataTable.DataTable({
         data: [],
+        drawCallback: function () {
+            this.api().columns().every( function () {
+                var column = this;
+                var select = $('<select><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                        );
+
+                        column
+                            .search( val ? '^'+val+'$' : '', true, false )
+                            .draw();
+                    } );
+
+                column.data().unique().sort().each( function ( d, j ) {
+                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                } );
+            } );
+        },
         columns: [
             {
                 data: 'course_id',
