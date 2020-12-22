088a1b30d00ba3ffebcfda846d4d01d20f9522c8
lumos <ankita008paul@gmail.com>
2020-12-22 12:34:47 +0530

    Student Courses dao

---
+++ src/main/java/com/iiitb/academia/dao/Student_CoursesDAO.java
@@ -0,0 +1,8 @@
+package com.iiitb.academia.dao;
+
+import com.iiitb.academia.bean.Student_Courses;
+
+public interface Student_CoursesDAO {
+
+    public abstract void addStudentCourses(Student_Courses student_courses);
+}
