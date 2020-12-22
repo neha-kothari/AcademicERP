ea55ce1631ae3ac2017feb2953aeb92265033734
Neha Kothari <37174450+neha-kothari@users.noreply.github.com>
2020-12-22 05:53:53 +0530

    Update login.js
    Changing it to login using Faculty email ID only

--- src/main/webapp/assets/js/login.js
+++ src/main/webapp/assets/js/login.js
@@ -1,31 +1,5 @@
 let form = document.getElementById('login-form');
 
-let getUserId = async (username, password) => {
-
-    if(username == "admin" && password=="password"){
-        return Promise.resolve("admin")
-    }
-    return Promise.resolve(null)
-
-    // Use this to check login in backend:
-    // const response = await fetch('api/students/login', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json;charset=utf-8'
-    //     },
-    //     body: JSON.stringify({
-    //         username: username,
-    //          password: password
-    //     })
-    // });
-    // let result = await response;
-    // if(result['status'] === 200){
-    //     let data = response.json();
-    //     return data['student_id']
-    // } else return null;
-
-};
-
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -33,15 +7,19 @@ form.addEventListener('submit', async (e) => {
         document.getElementById("submit-button").style.display = "none";
         document.getElementById("spinner-button").style.display = "block";
         const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
+        //const password = document.getElementById('password').value;
 
+        const response = await fetch('api/employee/email/'+username).catch(err => {
+            console.log(err)
+        });
 
-        const userId = await getUserId(username, password);
-        if(userId != null){
+        let result = await response;
+        if(result['status'] === 200){
+
+            //let data = response.json();
             document.getElementById("submit-button").style.display = "block";
             document.getElementById("spinner-button").style.display = "none";
 
-            sessionStorage.setItem('id', userId);
             location.href = "dashboard.html";
         }else{
             document.getElementById("submit-button").style.display = "block";
